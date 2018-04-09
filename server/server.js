59a104b156d2ed75a04a1520d74b174cca0801ae
Jon Maddox <jon@jonmaddox.com>
2018-04-08 23:12:56 -0400

    set up cors for non production

--- server/server.js
+++ server/server.js
@@ -20,6 +20,14 @@ const port = process.env.PORT || 5000
 app.use('/graphql', graphQLAPI)
 app.use(express.static(path.join(__dirname, 'public')))
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
+}
+
 app.get('/', (req, res) => {
   res.sendfile('index.html')
 })
