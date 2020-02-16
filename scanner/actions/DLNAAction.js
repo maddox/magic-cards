c10b0e03241c0e0a01fe6c0dbb7528a257d6f7d6
Eero Vilpponen <eero.vilpponen@gmail.com>
2020-02-16 20:41:07 +0200

    Fetch DLNA url every time

--- scanner/actions/DLNAAction.js
+++ scanner/actions/DLNAAction.js
@@ -1,15 +1,31 @@
 var exec = require('child_process').exec
 const MediaURLAction = require('./MediaURLAction')
+const pythonExtensionsPath = __dirname + '/../../python_extensions'
 
 class DLNAAction extends MediaURLAction {
   process() {
-    // Remove the DLNA title from this uri
-    this.card.uri = this.card.uri
-      .split(':')
-      .slice(1)
-      .join(':')
-    // Run as a normal media URL
-    super.process()
+    const envVars = this.envVars()
+    // Stop playback here for better UX
+    const scriptPath =
+      pythonExtensionsPath +
+      '/chromecast.py --chromecast_ip ' +
+      this.config.chromecast_name +
+      ' stop'
+    exec(scriptPath, {env: envVars}, function(error, stdout, stderr) {
+      console.log(stdout, stderr, error)
+    })
+    // Fetch all DLNA titles, and use the mappings to get a media URL
+    const scriptPath = pythonExtensionsPath + '/dlna.py --dlnaserver_ip ' + config.dlnaserver_ip
+    exec(scriptPath, (error, stdout, stderr) => {
+      if (error || stderr) {
+        console.log(error)
+      } else {
+        const dlnaMappings = JSON.parse(stdout)
+        this.card.uri = dlnaMappings[this.card.uri]
+        // Run as a normal media URL
+        super.process()
+      }
+    })
   }
 }
 
