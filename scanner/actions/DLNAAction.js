9cebdbb318115b98a1266864cd47ff3a7343c977
Eero Vilpponen <eero.vilpponen@gmail.com>
2020-02-17 12:17:53 +0200

    Fixes.

--- scanner/actions/DLNAAction.js
+++ scanner/actions/DLNAAction.js
@@ -1,27 +1,36 @@
 var exec = require('child_process').exec
 const MediaURLAction = require('./MediaURLAction')
-const pythonExtensionsPath = __dirname + '/../../python_extensions'
+const pythonExtensionsPath =
+  __dirname + '/../../.virtualenv/bin/python ' + __dirname + '/../../python_extensions'
 
 class DLNAAction extends MediaURLAction {
   process() {
     const envVars = this.envVars()
     // Stop playback here for better UX
-    const scriptPath =
-      pythonExtensionsPath +
-      '/chromecast.py --chromecast_ip ' +
-      this.config.chromecast_name +
-      ' stop'
-    exec(scriptPath, {env: envVars}, function(error, stdout, stderr) {
+    const scriptPathCC =
+      pythonExtensionsPath + '/chromecast.py --chromecast_ip ' + this.config.chromecast_ip + ' stop'
+    exec(scriptPathCC, {env: envVars}, function(error, stdout, stderr) {
       console.log(stdout, stderr, error)
     })
     // Fetch all DLNA titles, and use the mappings to get a media URL
-    const scriptPath = pythonExtensionsPath + '/dlna.py --dlnaserver_ip ' + config.dlnaserver_ip
-    exec(scriptPath, (error, stdout, stderr) => {
+    const scriptPathDLNA =
+      pythonExtensionsPath + '/dlna.py --dlnaserver_ip ' + this.config.dlnaserver_ip
+    exec(scriptPathDLNA, (error, stdout, stderr) => {
       if (error || stderr) {
         console.log(error)
       } else {
         const dlnaMappings = JSON.parse(stdout)
-        this.card.uri = dlnaMappings[this.card.uri]
+        const random = this.card.uri.split(':')[0]
+        const regex = this.card.uri.split(':')[1]
+        console.log('regex', regex)
+        // Allow using regex for getting multiple urls
+        this.card.uri =
+          random +
+          (random != '' ? ':' : '') +
+          Object.entries(dlnaMappings)
+            .filter(val => val[0].match(regex) !== null)
+            .map(val => val[1])
+            .join(' ')
         // Run as a normal media URL
         super.process()
       }
