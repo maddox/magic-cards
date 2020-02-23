8edf164adbe433f4928fbacb1f383d6fa50bff78
Eero Vilpponen <eero.vilpponen@gmail.com>
2020-02-23 20:53:04 +0200

    Format

--- scanner/actions/netflix_action.py
+++ scanner/actions/netflix_action.py
@@ -1,18 +1,17 @@
 import traceback
 
-from time import sleep
 from .action import ChromecastAction
 from utils.androidviewclient import Netflix
 
 
 class NetflixAction(ChromecastAction):
     def process(self):
-        if self.chromecast.running_app == 'netflix':
-            # TODO: Async needed here. If netflix is running, it needs to be stopped. But chromecast
-            # is really slow to first stop an app, then start another one (takes up to 10 seconds).
-            # Blocking for 10 seconds here is not something we want, so just "quit" netflix by running
-            # an empty file.
-            self.chromecast.play_media('http://localhost')
+        if self.chromecast.running_app == "netflix":
+            # TODO: Async needed here. If netflix is running, it needs to be stopped. But
+            # chromecast is really slow to first stop an app, then start another one (takes up to
+            # 10 seconds). Blocking for 10 seconds here is not something we want, so instead
+            # just "quit" netflix by running an empty file.
+            self.chromecast.play_media("http://localhost")
         else:
             # Start the netflix app, just for show (otherwise chromecast dashboard would load here
             # while we wait: Bad UI)
